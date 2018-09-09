83dda72d80ea71f09e34fda7c65beb82f7a2c87c
SaravananManickam2205 <sarava220592@gmail.com>
2018-09-09 22:49:00 +0530

    First Intial Code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AddtraineeComponent } from './addtrainee/addtrainee.component';
+import { DlDateTimePickerDateModule } from 'angular-bootstrap-datetimepicker';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AddtraineeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DlDateTimePickerDateModule,
+    NgbModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path : 'addTrainee', component : AddtraineeComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/trainee.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpService } from '@angular/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TraineeService {
+
+  constructor(private httpService:HttpService){
+
+  }
+
+  addTraineeDetails(){
+
+  }
+
+}
